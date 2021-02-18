39dc955d3a2d74c4b6891f28611be4fb8e3459e7
NOBODY43865 <79256955+NOBODY43865@users.noreply.github.com>
2021-02-18 20:17:54 +0800

    Add files via upload

---
+++ say hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>//"打招呼"
+
+int main()//主函数main-程序的入口-main函数只能有一个
+	//int 表示整型
+{//{}内完成任务
+	printf("hello world");//printf为打印函数，是表示在屏幕上打字的指令
+	//库函数：printf；为使用库函数需要“打招呼”-#include<stdio.h>
+	return 0;//返回 0
+}
