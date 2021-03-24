a39817836872dca3c004b2298e2fd891ad81b002
NOBODY43865 <79256955+NOBODY43865@users.noreply.github.com>
2021-03-24 10:50:51 +0800

    Add files via upload

---
+++ 练习3.cpp
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+////好好学习，未来机会
+////家里有矿，呵呵呵呵
+////不去努力，去卖红薯
+//#include<stdio.h>
+//int main()
+//{
+//	int age;
+//	scanf("%d",&age);
+//	if(age<18)
+//		printf("未成年\n");
+//	else if(age>=18 && age<28)
+//		printf("青年\n");
+//	else if(age>=28 && age<50)
+//		printf("壮年\n");
+//	else
+//		printf("老不死\n");
+//	return 0;
+//}
+
+//else就近原则
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if(a == 1)
+//		if(b == 2)
+//			printf("hehe\n");
+//		else
+//			printf("haha\n");
+//	return 0;
+//
+//}
+
+////输出1-100之间的奇数
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 1;
+//	while(a<=100)
+//	{
+//		if(a%2)
+//		{
+//			printf("%d\n",a);
+//			a++;
+//		}
+//		else
+//			a++;
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d",&day);
+//	switch(day)
+//	{
+//	case 1:
+//			printf("星期1\n");
+//			break;
+//	case 2:
+//			printf("星期2\n");
+//			break;
+//	case 3:
+//			printf("星期3\n");
+//			break;
+//	case 4:
+//			printf("星期4\n");
+//			break;
+//	case 5:
+//			printf("星期5\n");
+//			break;
+//	case 6:
+//			printf("星期6\n");
+//			break;
+//	case 7:
+//			printf("星期天\n");
+//			break;
+//	}
+//	return 0;
+//}
+
