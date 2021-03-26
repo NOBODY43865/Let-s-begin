7354fabad24b997f060e690975de3853d9edb723
NOBODY43865 <79256955+NOBODY43865@users.noreply.github.com>
2021-03-26 20:27:01 +0800

    Add files via upload

---
+++ 练习5.cpp
@@ -0,0 +1,74 @@
+// 练习5.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//int ch = 0;
+	//while((ch = getchar()) !=EOF)
+	//{
+	//	putchar(ch);
+	//}
+
+	////密码输入
+	//char password[20] = {0};
+	//int ret = 0;
+	//int ch = 0;
+	//printf("输入密码:>");
+	//scanf("%s",password);
+	////缓冲区剩余\n
+	////getchar();
+	//while((ch = getchar())!='\n')
+	//{
+	//	;
+	//}
+	//printf("请确认（Y/N）:>");
+	//ret = getchar();
+	//if (ret =='Y')
+	//{
+	//	printf("确认成功\n");
+	//}
+	//else
+	//{
+	//	printf("放弃确认\n");
+	//}
+
+	////计算n的阶乘
+	//int n = 0;
+	//printf("请输入需要计算的n的值\n");
+	//scanf("%d",&n);
+	//int i = 1;
+	//int aim = 1;
+	//for(i=1;i<=n;i++)
+	//{
+	//	aim = aim * i;
+	//}
+	//printf("aim = %d\n",aim);
+
+	//int n = 0;
+	//printf("请输入需要计算的n的值\n");
+	//scanf("%d",&n);
+	//int i = 1;
+	////int j = 1;
+	//int ret = 1;
+	//int sum = 0;
+	//for(i=1;i<=n;i++)
+	//{
+	//	/*ret=1;
+	//	for(j=1;j<=i;j++)
+	//	{*/
+	//		ret=ret*i/*j*/;
+	//	/*}*/
+	//	sum = sum + ret;
+	//}
+	//printf("sum = %d\n",sum);
+
+	int arr[10]={1,2,3,4,5,6,7,8,9};
+	int k = 7;
+	//写代码在arr数组中找到7
+
+	return 0;
+}
+
